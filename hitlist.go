78fd1c7ea904d301bd6537a7c57217750e219300
LOZORD <ljr@google.com>
2017-12-09 18:25:15 -0500

    Added Twitter/anaconda integration.

--- hitlist.go
+++ hitlist.go
@@ -14,38 +14,71 @@ import (
 	"os/user"
 	"path/filepath"
 
+	"github.com/chimeracoder/anaconda"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
 	sheets "google.golang.org/api/sheets/v4"
 )
 
 var (
+	// Sheets flags.
 	clientSecretFilePathFlag = flag.String("client_secret_file", "./client_secret.json", "the path of of the Sheets client secret file")
 	spreadsheetIDFlag        = flag.String("sheet_id", "", "the id of the spreadsheet to read")
 	sheetNameFlag            = flag.String("sheet_name", "Sheet1", "the name of the sheet from which to read")
 	readRangeFlag            = flag.String("read_range", "", "the range to read from the sheet (e.g. 'A2:E')")
+	// Twitter flags.
+	consumerKeyFlag    = flag.String("twitter_consumer_key", "", "the consumer key for the Twitter account")
+	consumerSecretFlag = flag.String("twitter_consumer_secret", "", "the consumer secret for the Twitter account")
+	accessTokenFlag    = flag.String("twitter_access_token", "", "the access token for the Twitter account")
+	accessSecretFlag   = flag.String("twitter_access_secret", "", "the access token secret for the Twitter account")
 )
+
+type sheetsConfig struct {
+	secretPath, id, name, cellRange string
+}
+
+type twitterConfig struct {
+	consumerKey, consumerSecret string
+	accessToken, accessSecret   string
+}
 
 // This code is inspired by the guide here:
 // https://developers.google.com/sheets/api/quickstart/go
 
 func main() {
 	flag.Parse()
-	if err := doMain(*clientSecretFilePathFlag, *spreadsheetIDFlag, *sheetNameFlag, *readRangeFlag); err != nil {
+
+	sc := &sheetsConfig{
+		secretPath: *clientSecretFilePathFlag,
+		id:         *spreadsheetIDFlag,
+		name:       *sheetNameFlag,
+		cellRange:  *readRangeFlag,
+	}
+
+	tc := &twitterConfig{
+		consumerKey:    *consumerKeyFlag,
+		consumerSecret: *consumerSecretFlag,
+		accessToken:    *accessTokenFlag,
+		accessSecret:   *accessSecretFlag,
+	}
+
+	if err := doMain(sc, tc); err != nil {
 		log.Fatal(err)
 	}
 }
 
-func doMain(secretPath, spreadsheetID, sheetName, readRange string) error {
+const permScope = "https://www.googleapis.com/auth/spreadsheets.readonly"
+
+func doMain(sc *sheetsConfig, tc *twitterConfig) error {
 	ctx := context.Background()
-	secretContent, err := ioutil.ReadFile(secretPath)
+	secretContent, err := ioutil.ReadFile(sc.secretPath)
 	if err != nil {
 		return fmt.Errorf("failed to read client secret file: %v", err)
 	}
 
-	config, err := google.ConfigFromJSON(secretContent, "https://www.googleapis.com/auth/spreadsheets.readonly")
+	config, err := google.ConfigFromJSON(secretContent, permScope)
 	if err != nil {
-		return fmt.Errorf("failed to create config from secret file at %q: %v", secretPath, err)
+		return fmt.Errorf("failed to create config from secret file at %q: %v", sc.secretPath, err)
 	}
 
 	client, err := getClient(ctx, config)
@@ -58,17 +91,21 @@ func doMain(secretPath, spreadsheetID, sheetName, readRange string) error {
 		return fmt.Errorf("failed to retrieve client for Sheets: %v", err)
 	}
 
-	readRange = fmt.Sprintf("%s!%s", sheetName, readRange)
-	resp, err := srv.Spreadsheets.Values.Get(spreadsheetID, readRange).Do()
+	r := fmt.Sprintf("%s!%s", sc.name, sc.cellRange)
+	resp, err := srv.Spreadsheets.Values.Get(sc.id, r).Do()
 	if err != nil {
-		return fmt.Errorf("failed to read sheet with id=%q and range=%q: %v", spreadsheetID, readRange, err)
+		return fmt.Errorf("failed to read sheet with id=%q and range=%q: %v", sc.id, r, err)
 	}
 
 	if len(resp.Values) < 1 {
 		return errors.New("no data found from spreadsheet")
 	}
 
-	return tweet(resp.Values)
+	anaconda.SetConsumerKey(tc.consumerKey)
+	anaconda.SetConsumerKey(tc.consumerSecret)
+	api := anaconda.NewTwitterApi(tc.accessToken, tc.accessSecret)
+
+	return tweet(api, resp.Values)
 }
 
 func getClient(ctx context.Context, config *oauth2.Config) (*http.Client, error) {
@@ -133,7 +170,15 @@ func saveToken(file string, token *oauth2.Token) {
 	json.NewEncoder(f).Encode(token)
 }
 
-func tweet(data interface{}) error {
+func tweet(api *anaconda.TwitterApi, data interface{}) error {
 	log.Printf("would have tweeted data: %v", data)
+	status := fmt.Sprintf("some cool data: %v", data)
+	if len(status) > 280 {
+		status = status[:280]
+	}
+	if _, err := api.PostTweet(status, url.Values{}); err != nil {
+		return err
+	}
+
 	return nil
 }
